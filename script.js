84640271955c0d2651943291d21ddb81f204b8bd
Shaun Kelly <rawtech@Shauns-iMac.lan>
2023-06-29 16:24:52 -0700

    updated JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+const submitButton = document.querySelector('.submit-button');
+const errorContainer = document.querySelector('.error-container')
+
+
+
+submitButton.addEventListener('click', function(){
+    let errorMessageText = "Please enter a valid email address";
+
+   errorContainer.textContent = errorMessageText;
+   
+});
